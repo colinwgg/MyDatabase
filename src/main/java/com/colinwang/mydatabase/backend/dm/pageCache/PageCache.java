31a896c60ef62c68f000f4ed88252d69b31a5d05
Colin Wang <2802235800@qq.com>
2024-09-15 12:57:23 +0800

    MyDatabase-数据页-实现页面和页面缓存

--- src/main/java/com/colinwang/mydatabase/backend/dm/pageCache/PageCache.java
+++ src/main/java/com/colinwang/mydatabase/backend/dm/pageCache/PageCache.java
@@ -1,6 +1,13 @@
 package com.colinwang.mydatabase.backend.dm.pageCache;
 
 import com.colinwang.mydatabase.backend.dm.page.Page;
+import com.colinwang.mydatabase.backend.utils.Panic;
+import com.colinwang.mydatabase.common.Error;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
 
 public interface PageCache {
 
@@ -14,4 +21,48 @@ public interface PageCache {
     void truncateByBgno(int maxPgno);
     int getPageNumber();
     void flushPage(Page pg);
+
+    public static PageCacheImpl create(String path, long memory) {
+        File f = new File(path+PageCacheImpl.DB_SUFFIX);
+        try {
+            if(!f.createNewFile()) {
+                Panic.panic(Error.FileExistsException);
+            }
+        } catch (Exception e) {
+            Panic.panic(e);
+        }
+        if(!f.canRead() || !f.canWrite()) {
+            Panic.panic(Error.FileCannotRWException);
+        }
+
+        FileChannel fc = null;
+        RandomAccessFile raf = null;
+        try {
+            raf = new RandomAccessFile(f, "rw");
+            fc = raf.getChannel();
+        } catch (FileNotFoundException e) {
+            Panic.panic(e);
+        }
+        return new PageCacheImpl(raf, fc, (int)memory/PAGE_SIZE);
+    }
+
+    public static PageCacheImpl open(String path, long memory) {
+        File f = new File(path+PageCacheImpl.DB_SUFFIX);
+        if(!f.exists()) {
+            Panic.panic(Error.FileNotExistsException);
+        }
+        if(!f.canRead() || !f.canWrite()) {
+            Panic.panic(Error.FileCannotRWException);
+        }
+
+        FileChannel fc = null;
+        RandomAccessFile raf = null;
+        try {
+            raf = new RandomAccessFile(f, "rw");
+            fc = raf.getChannel();
+        } catch (FileNotFoundException e) {
+            Panic.panic(e);
+        }
+        return new PageCacheImpl(raf, fc, (int)memory/PAGE_SIZE);
+    }
 }
