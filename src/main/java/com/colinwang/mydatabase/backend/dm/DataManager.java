07d163e11d7d556961ad084efceb99fdda96c9aa
Colin Wang <2802235800@qq.com>
2025-01-25 14:26:47 +0800

    MyDatabase-DataManager初步实现

--- src/main/java/com/colinwang/mydatabase/backend/dm/DataManager.java
+++ src/main/java/com/colinwang/mydatabase/backend/dm/DataManager.java
@@ -1,5 +1,36 @@
 package com.colinwang.mydatabase.backend.dm;
 
-public interface DataManager {
+import com.colinwang.mydatabase.backend.dm.DataItem.DataItem;
+import com.colinwang.mydatabase.backend.dm.DataItem.DataItemImpl;
+import com.colinwang.mydatabase.backend.dm.logger.Logger;
+import com.colinwang.mydatabase.backend.dm.page.PageOne;
+import com.colinwang.mydatabase.backend.dm.pageCache.PageCache;
+import com.colinwang.mydatabase.backend.dm.pageCache.PageCacheImpl;
+import com.colinwang.mydatabase.backend.tm.TransactionManager;
 
+public interface DataManager {
+    DataItem read(long uid) throws Exception;
+    long insert(long xid, byte[] data) throws Exception;
+    void close();
+
+    public static DataManagerImpl create(String path, long mem, TransactionManager tm) {
+        PageCache pc = PageCache.create(path, mem);
+        Logger log = Logger.create(path);
+        DataManagerImpl dm = new DataManagerImpl(pc, log, tm);
+        dm.initPageOne();
+        return dm;
+    }
+
+    public static DataManagerImpl open(String path, long mem, TransactionManager tm) {
+        PageCacheImpl pc = PageCache.open(path, mem);
+        Logger log = Logger.open(path);
+        DataManagerImpl dm = new DataManagerImpl(pc, log, tm);
+        if (!dm.loadCheckPageOne()) {
+            Recover.recover(tm, log, pc);
+        }
+        dm.fillPageIndex();
+        PageOne.setVcOpen(dm.pageOne);
+        dm.pc.flushPage(dm.pageOne);
+        return dm;
+    }
 }
--- src/main/java/com/colinwang/mydatabase/backend/dm/DataManagerImpl.java
+++ src/main/java/com/colinwang/mydatabase/backend/dm/DataManagerImpl.java
@@ -2,12 +2,19 @@ package com.colinwang.mydatabase.backend.dm;
 
 import com.colinwang.mydatabase.backend.common.AbstractCache;
 import com.colinwang.mydatabase.backend.dm.DataItem.DataItem;
+import com.colinwang.mydatabase.backend.dm.DataItem.DataItemImpl;
 import com.colinwang.mydatabase.backend.dm.logger.Logger;
 import com.colinwang.mydatabase.backend.dm.page.Page;
+import com.colinwang.mydatabase.backend.dm.page.PageOne;
+import com.colinwang.mydatabase.backend.dm.page.PageX;
 import com.colinwang.mydatabase.backend.dm.pageCache.PageCache;
 import com.colinwang.mydatabase.backend.dm.pageIndex.PageIndex;
 import com.colinwang.mydatabase.backend.tm.TransactionManager;
+import com.colinwang.mydatabase.backend.utils.Panic;
 
+/**
+ * DM层对外提供方法的类 & DataItem对象的缓存
+ */
 public class DataManagerImpl extends AbstractCache<DataItem> implements DataManager {
 
     TransactionManager tm;
@@ -16,25 +23,87 @@ public class DataManagerImpl extends AbstractCache<DataItem> implements DataMana
     PageIndex pIndex;
     Page pageOne;
 
-    public DataManagerImpl(int maxResource) {
-        super(maxResource);
+    public DataManagerImpl(PageCache pc, Logger logger, TransactionManager tm) {
+        super(0);
+        this.pc = pc;
+        this.logger = logger;
+        this.tm = tm;
+        this.pIndex = new PageIndex();
     }
 
     @Override
-    protected DataItem getForCache(long key) throws Exception {
-        return null;
+    protected DataItem getForCache(long uid) throws Exception {
+        short offset = (short) (uid & ((1L << 16) - 1));
+        uid >>>= 32;
+        int pgno = (int) (uid & ((1L << 32) - 1));
+        Page pg = pc.getPage(pgno);
+        return DataItem.parseDataItem(pg, offset, this);
     }
 
     @Override
-    protected void releaseForCache(DataItem obj) {
-
+    protected void releaseForCache(DataItem di) {
+        di.page().release();
     }
 
+    // 为xid生成update日志
     public void logDataItem(long xid, DataItem di) {
-
+        byte[] log = Recover.updateLog(xid, di);
+        logger.log(log);
     }
 
     public void releaseDataItem(DataItem di) {
         super.release(di.getUid());
+    }
+
+    @Override
+    public DataItem read(long uid) throws Exception {
+        return null;
+    }
+
+    @Override
+    public long insert(long xid, byte[] data) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    // 创建文件时初始化PageOne
+    void initPageOne() {
+        int pgno = pc.newPage(PageOne.InitRaw());
+        assert pgno == 1;
+        try {
+            pageOne = pc.getPage(pgno);
+        } catch (Exception e) {
+            Panic.panic(e);
+        }
+        pc.flushPage(pageOne);
+    }
+
+    // 在打开已有文件时时读入PageOne，并验证正确性
+    boolean loadCheckPageOne() {
+        try {
+            pageOne = pc.getPage(1);
+        } catch (Exception e) {
+            Panic.panic(e);
+        }
+        return PageOne.checkVc(pageOne);
+    }
+
+    // 初始化pageIndex
+    void fillPageIndex() {
+        int pageNumber = pc.getPageNumber();
+        for (int i = 2; i <= pageNumber; i++) {
+            Page pg = null;
+            try {
+                pg = pc.getPage(i);
+            } catch (Exception e) {
+                Panic.panic(e);
+            }
+            pIndex.add(pg.getPageNumber(), PageX.getFreeSpace(pg));
+            pg.release();
+        }
     }
 }
