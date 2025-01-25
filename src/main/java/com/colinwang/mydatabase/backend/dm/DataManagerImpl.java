c57ea8029de13f18504a82b4210b69d7b172e8cd
Colin Wang <2802235800@qq.com>
2025-01-25 15:07:38 +0800

    MyDatabase-DataManager实现读，插入

--- src/main/java/com/colinwang/mydatabase/backend/dm/DataManagerImpl.java
+++ src/main/java/com/colinwang/mydatabase/backend/dm/DataManagerImpl.java
@@ -9,8 +9,11 @@ import com.colinwang.mydatabase.backend.dm.page.PageOne;
 import com.colinwang.mydatabase.backend.dm.page.PageX;
 import com.colinwang.mydatabase.backend.dm.pageCache.PageCache;
 import com.colinwang.mydatabase.backend.dm.pageIndex.PageIndex;
+import com.colinwang.mydatabase.backend.dm.pageIndex.PageInfo;
 import com.colinwang.mydatabase.backend.tm.TransactionManager;
 import com.colinwang.mydatabase.backend.utils.Panic;
+import com.colinwang.mydatabase.backend.utils.Types;
+import com.colinwang.mydatabase.common.Error;
 
 /**
  * DM层对外提供方法的类 & DataItem对象的缓存
@@ -57,17 +60,67 @@ public class DataManagerImpl extends AbstractCache<DataItem> implements DataMana
 
     @Override
     public DataItem read(long uid) throws Exception {
-        return null;
+        DataItemImpl di = (DataItemImpl) super.get(uid);
+        if (!di.isValid()) {
+            di.release();
+            return null;
+        }
+        return di;
     }
 
     @Override
     public long insert(long xid, byte[] data) throws Exception {
-        return 0;
+        byte[] raw = DataItem.wrapDataItemRaw(data);
+        if (raw.length > PageX.MAX_FREE_SPACE) {
+            throw Error.DataTooLargeException;
+        }
+
+        PageInfo pi = null;
+        for (int i = 0; i < 5; i++) {
+            // 尝试从页面索引获取可用页面
+            pi = pIndex.select(raw.length);
+            if (pi != null) {
+                break;
+            } else {
+                // 获取不到则生成新的空页面并添加到页面索引
+                int newPgno = pc.newPage(PageX.iniRaw());
+                pIndex.add(newPgno, PageX.MAX_FREE_SPACE);
+            }
+        }
+        if (pi == null) {
+            throw Error.DatabaseBusyException;
+        }
+
+        Page pg = null;
+        int defaultFreeSpace = 0;
+        try {
+            pg = pc.getPage(pi.pgno);
+            // 先写入insert日志
+            byte[] log = Recover.insertLog(xid, pg, raw);
+            logger.log(log);
+            // 接下来插入日志并获取offset
+            short offset = PageX.insert(pg, raw);
+
+            pg.release();
+            return Types.addressToUid(pi.pgno, offset);
+        } finally {
+            if (pg != null) {
+                // 最后需要将pg重新插入页面索引
+                pIndex.add(pi.pgno, PageX.getFreeSpace(pg));
+            } else {
+                pIndex.add(pi.pgno, defaultFreeSpace);
+            }
+        }
     }
 
     @Override
     public void close() {
+        super.close();
+        logger.close();
 
+        PageOne.setVcClose(pageOne);
+        pageOne.release();
+        pc.close();
     }
 
     // 创建文件时初始化PageOne
