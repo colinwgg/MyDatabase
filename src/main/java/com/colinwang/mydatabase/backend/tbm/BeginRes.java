1c9746825fbb66d3ed28776068ec20166bf0c291
Colin Wang <2802235800@qq.com>
2025-02-15 18:59:21 +0800

    MyDatabase-实现TableManager

---
+++ src/main/java/com/colinwang/mydatabase/backend/tbm/BeginRes.java
@@ -0,0 +1,6 @@
+package com.colinwang.mydatabase.backend.tbm;
+
+public class BeginRes {
+    public long xid;
+    public byte[] result;
+}
---
+++ src/main/java/com/colinwang/mydatabase/backend/tbm/FieldCalRes.java
@@ -0,0 +1,6 @@
+package com.colinwang.mydatabase.backend.tbm;
+
+public class FieldCalRes {
+    public long left;
+    public long right;
+}
